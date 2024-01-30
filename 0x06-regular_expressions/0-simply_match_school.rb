c9b928e2b22a164571faf7932b10cfcc3104813e
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-01-30 22:02:00 +0200

    done

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -2,12 +2,13 @@
 # Making my own Expression!
 
 i = 0
+k = ""
 while ARGV[i]
     ARGV[i].split.each do |word|
         if word == "School"
-            print "School"
+            k += "School"
         end
     end
     i += 1
 end
-puts ""
+puts k
