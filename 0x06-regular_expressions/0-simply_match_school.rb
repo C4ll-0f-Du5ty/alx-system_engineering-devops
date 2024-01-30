bf70ac979d3a7fbb606c55cb18c84ac0800730f9
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-01-30 22:08:50 +0200

    done

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -2,13 +2,5 @@
 # Making my own Expression!
 
 i = 0
-k = ""
-while ARGV[i]
-    ARGV[i].split.each do |word|
-        if word == "School"
-            k += "School"
-        end
-    end
-    i += 1
-end
+k = ARGV[0].scan(/School/).join
 puts k
