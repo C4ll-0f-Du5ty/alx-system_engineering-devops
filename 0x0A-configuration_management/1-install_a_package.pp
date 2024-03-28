b460f66cb675382d7c97a43da2bf62ec8e987886
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-03-28 04:39:22 +0200

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,19 +1,5 @@
 # installing Flask using Puppet
 package { 'flask':
-  ensure   => installed,
-}
-
-package { 'Werkzeug':
-  ensure   => '2.1.1'
-  provider => 'pip3'
-}
-
-package { 'Python':
-  ensure => installed
-}
-
-exec { 'ACTION':
-  command => '/usr/bin/pip3 install flask=2.1.0',
-  path    => ['/usr/bin/', '/usr/sbin/'],
-  require => [package['Werkzeug'], package['python3']],
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
