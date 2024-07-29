6cf2f48bb154bc4c35cf2515096af0a760fe4e1c
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-07-29 18:34:32 +0300

    done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,22 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 import requests
 import sys
 
 
+url = "https://jsonplaceholder.typicode.com/"
 employee_ID = sys.argv[1]
-response1 = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_ID}')
+response1 = requests.get(url + "users/" + employee_ID)
 r1 = response1.json()
 
 params = {"userId": employee_ID}
 
-response2 = requests.get(f"https://jsonplaceholder.typicode.com/todos", params)
+response2 = requests.get(url + "/todos", params)
 r2 = response2.json()
 
 completed = []
 
 for t in r2:
-    if t['completed'] == True:
+    if t['completed'] is True:
         completed.append(t['title'])
 
 print(f"Employee {r1['name']} is done with tasks({len(completed)}/{len(r2)}):")
