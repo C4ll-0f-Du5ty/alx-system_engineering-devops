31cf6d44b6adab7e850e66a50ea640d73d6ab7ef
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-07-29 18:37:02 +0300

    done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import requests
 import sys
-
+"""Gathering Some information and Presenting them in specific foramt"""
 
 url = "https://jsonplaceholder.typicode.com/"
 employee_ID = sys.argv[1]
