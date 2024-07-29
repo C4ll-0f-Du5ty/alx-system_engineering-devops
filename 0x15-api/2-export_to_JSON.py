8a81f339074d40bbe891ac7e1efb0ed0ca78c2cf
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-07-29 20:19:21 +0300

    done

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,27 +4,21 @@ extend your Python script to export data in the JSON format."""
 import requests
 import sys
 
-
 if __name__ == "__main__":
-
     url = "https://jsonplaceholder.typicode.com/"
     employee_ID = sys.argv[1]
     response1 = requests.get(url + "users/" + employee_ID)
     r1 = response1.json()
 
     params = {"userId": employee_ID}
-
-    response2 = requests.get(url + "/todos", params)
+    response2 = requests.get(url + "/todos", params=params)
     r2 = response2.json()
 
     file = f"{employee_ID}.json"
-    Data = ['{"task": ' + '"{2}", "completed": "{1}", "username": "{0}"'
-            .format(r1.get("username"),
-                    r.get("completed"),
-                    r.get("title")) + '}' for r in r2
-            ]
-    D = ", ".join(Data)
-    with open(file, mode='w', newline='', encoding='utf-8') as f:
-        f.write('{"' + employee_ID + '": [')
+    Data = ['{"task": "{}", "completed": {}, "username": "{}"'.
+            format(r.get("title"), r.get("completed"), r1.get("username")) for r in r2]
+    D = ',\n'.join(Data)  # Join items with a comma and newline for readability
+    with open(file, mode='w', encoding='utf-8') as f:
+        f.write('[\n')
         f.write(D)
-        f.write(']}')
+        f.write('\n]')
