c43921ac1711d738404e8aef9cf1cfd000ef44a5
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-07-29 20:29:20 +0300

    done

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Using what you did in the task #0,
 extend your Python script to export data in the JSON format."""
+import json
 import requests
 import sys
 
@@ -18,13 +19,8 @@ if __name__ == "__main__":
     r2 = response2.json()
 
     file = f"{employee_ID}.json"
-    Data = ['{"task": ' + '"{2}", "completed": "{1}", "username": "{0}"'
-            .format(r1.get("username"),
-                    r.get("completed"),
-                    r.get("title")) + '}' for r in r2
-            ]
-    D = ", ".join(Data)
     with open(file, mode='w', newline='', encoding='utf-8') as f:
-        f.write('{"' + employee_ID + '": [')
-        f.write(D)
-        f.write(']}')
+        json.dump({employee_ID: [{
+            "task": r.get("title"),
+            "completed": r.get("completed"),
+            "username": r1.get("username")} for r in r2]}, f)
