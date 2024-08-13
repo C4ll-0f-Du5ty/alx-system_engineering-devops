b336508f0418dfa5eb86f0eb42c51f1aa55e8859
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-08-13 17:49:32 +0300

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,6 +8,7 @@ def number_of_subscribers(subreddit):
     returns the number of subscribers (not active users,
     total subscribers) for a given subreddit.
     If an invalid subreddit is given, the function should return 0."""
+
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
