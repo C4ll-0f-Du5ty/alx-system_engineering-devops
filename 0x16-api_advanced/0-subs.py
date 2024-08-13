f1b6bcb9e836b91f5b9055bbb447b7acfc4fe2b4
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-08-13 17:48:23 +0300

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,6 +4,10 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """Write a function that queries the Reddit API and
+    returns the number of subscribers (not active users,
+    total subscribers) for a given subreddit.
+    If an invalid subreddit is given, the function should return 0."""
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
