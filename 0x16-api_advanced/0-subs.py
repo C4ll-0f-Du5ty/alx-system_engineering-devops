0c727f682e2a8bfe1fb11d1f12a307f307fd85ce
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-08-13 17:45:27 +0300

    done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,9 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    response = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json")
+    response = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/about.json",
+        allow_redirects=False)
     r2 = response.json()
     if response.status_code == 200:
         return (r2.get("data").get("subscribers"))
